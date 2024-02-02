6afadc40ae51c53a5a9b8ccfbb219601b9136e8a
Bit7 <156470221+Bit7cr@users.noreply.github.com>
2024-02-02 22:25:57 +0100

    Add files via upload

---
+++ bit7-auto.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+sudo apt-get update && sudo apt-get upgrade -y
+
+sudo apt-get install libxcb-xinerama0 -y
+
+cd $HOME
+
+wget "https://dl.walletbuilders.com/download?customer=d4cd836283cc39673f6a843db5c41e3344c34d3ae13f297bfa&filename=bit7-qt-linux.tar.gz" -O bit7-qt-linux.tar.gz
+
+mkdir $HOME/Desktop/Bit7
+
+tar -xzvf bit7-qt-linux.tar.gz --directory $HOME/Desktop/Bit7
+
+mkdir $HOME/.bit7
+
+cat << EOF > $HOME/.bit7/bit7.conf
+rpcuser=rpc_bit7
+rpcpassword=dR2oBQ3K1zYMZQtJFZeAerhWxaJ5Lqeq9J2
+rpcbind=127.0.0.1
+rpcallowip=127.0.0.1
+listen=1
+server=1
+addnode=node3.walletbuilders.com
+EOF
+
+cat << EOF > $HOME/Desktop/Bit7/start_wallet.sh
+#!/bin/bash
+SCRIPT_PATH=\`pwd\`;
+cd \$SCRIPT_PATH
+./bit7-qt
+EOF
+
+chmod +x $HOME/Desktop/Bit7/start_wallet.sh
+
+cat << EOF > $HOME/Desktop/Bit7/mine.sh
+#!/bin/bash
+SCRIPT_PATH=\`pwd\`;
+cd \$SCRIPT_PATH
+while :
+do
+./bit7-cli generatetoaddress 1 \$(./bit7-cli getnewaddress)
+done
+EOF
+
+chmod +x $HOME/Desktop/Bit7/mine.sh
+    
+exec $HOME/Desktop/Bit7/bit7-qt &
+
+sleep 15
+
+exec $HOME/Desktop/Bit7/bit7-cli -named createwallet wallet_name="" &
+    
+sleep 15
+
+cd $HOME/Desktop/Bit7/
+
+clear
+
+exec $HOME/Desktop/Bit7/mine.sh
